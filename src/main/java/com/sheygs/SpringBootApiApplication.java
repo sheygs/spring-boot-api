066664ed7c02fc730b4a33bf4011ba3cda4dfba1
Olusegun Ekoh <segun.ekoh@gmail.com>
2025-08-09 12:35:58 +0100

    chore(setup): Bootstrap Application

---
+++ src/main/java/com/sheygs/SpringBootApiApplication.java
@@ -0,0 +1,20 @@
+package com.sheygs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApiApplication.class, args);
+    }
+
+    @GetMapping
+    public String getMessage(){
+        return "Hello, World!";
+    }
+}
