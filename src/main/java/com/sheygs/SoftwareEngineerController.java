7debf6e0accd800a8d6631100eb1026eea8e6af4
Olusegun Ekoh <segun.ekoh@gmail.com>
2025-08-10 13:17:28 +0100

    feat: Implement CRUD operations

--- src/main/java/com/sheygs/SoftwareEngineerController.java
+++ src/main/java/com/sheygs/SoftwareEngineerController.java
@@ -1,8 +1,6 @@
 package com.sheygs;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,8 +14,33 @@ public class SoftwareEngineerController {
         this.softwareEngineerService = softwareEngineerService;
     }
 
+    // use DTOs
     @GetMapping
     public List<SoftwareEngineer> getEngineers(){
         return softwareEngineerService.getEngineers();
+    }
+
+    // use DTOs
+    @PostMapping
+    public void addEngineer(@RequestBody SoftwareEngineer softwareEngineer){
+      softwareEngineerService.insertEngineer(softwareEngineer);
+    }
+
+    // use DTOs
+    @GetMapping("{id}")
+    public SoftwareEngineer getEngineerById(@PathVariable Integer id){
+      return this.softwareEngineerService.getEngineerById(id);
+    }
+
+    // use DTOs
+    @DeleteMapping("{id}")
+    public SoftwareEngineer deleteEngineerById(@PathVariable Integer id){
+        return this.softwareEngineerService.removeEngineer(id);
+    }
+
+    // use DTOs
+    @PutMapping("{id}")
+    public SoftwareEngineer updateEngineer(@PathVariable Integer id, @RequestBody SoftwareEngineer softwareEngineer){
+        return this.softwareEngineerService.updateEngineer(id, softwareEngineer);
     }
 }
--- src/main/java/com/sheygs/SoftwareEngineerService.java
+++ src/main/java/com/sheygs/SoftwareEngineerService.java
@@ -1,5 +1,6 @@
 package com.sheygs;
 
+
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,8 +13,45 @@ public class SoftwareEngineerService {
          this.softwareEngineerRepository = softwareEngineerRepository;
     }
 
-    // preferred: use a DTO
+    // preferred: use DTO
     public List<SoftwareEngineer> getEngineers(){
       return this.softwareEngineerRepository.findAll();
+    }
+
+    // preferred: use DTO
+    public void insertEngineer(SoftwareEngineer softwareEngineer) {
+      this.softwareEngineerRepository.save(softwareEngineer);
+    }
+
+    // preferred: use DTO
+    public SoftwareEngineer getEngineerById(Integer id) {
+        return this.softwareEngineerRepository.findById(id).orElseThrow(() -> new IllegalStateException(id + " not found"));
+    }
+
+    // preferred: use DTO
+    public SoftwareEngineer removeEngineer(Integer id) {
+        var engineer = this.getEngineerById(id);
+
+        if (engineer == null) {
+            throw new IllegalArgumentException(id + " not found");
+        }
+
+        this.softwareEngineerRepository.deleteById(engineer.getId());
+
+        return engineer;
+    }
+
+    // preferred: use DTO
+    public SoftwareEngineer updateEngineer(Integer id, SoftwareEngineer softwareEngineer) {
+        var engineer = this.getEngineerById(id);
+
+        if (engineer == null) {
+            throw new IllegalArgumentException(id + " not found");
+        }
+
+        engineer.setName(softwareEngineer.getName());
+        engineer.setTechStack(softwareEngineer.getTechStack());
+
+        return this.softwareEngineerRepository.save(engineer);
     }
 }
