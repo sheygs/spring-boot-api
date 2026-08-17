eb62c02e0fe4217c2e6ad8d526045265b77e185e
Olusegun Ekoh <segun.ekoh@gmail.com>
2026-08-17 13:36:56 +0100

    chore: Family Cash Card

---
+++ src/main/java/com/olusegun/cashcard/CashcardApplication.java
@@ -0,0 +1,11 @@
+package com.olusegun.cashcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashcardApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CashcardApplication.class, args);
+	}
+}
